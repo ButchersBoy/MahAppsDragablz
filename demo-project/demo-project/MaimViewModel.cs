9540717c2b00399fb6d12e0d5414cfc57f6d1fd6
bbarry <sidbouba@gmail.com>
2015-04-02 22:36:06 +0200

    add main view model

---
+++ demo-project/demo-project/MaimViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace demo_project
+{
+    class MaimViewModel
+    {
+        public UserControl CurrentDisplayedView { get; private set; }
+
+        public MaimViewModel()
+        {
+        }
+
+
+    }
+}
